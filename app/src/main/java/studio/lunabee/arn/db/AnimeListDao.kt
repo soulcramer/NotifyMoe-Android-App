4d4c504a6d01c8c32faa33f475276e033968f311
Scott <Soulcramer@gmail.com>
2018-05-25 10:51:07 +0200

    ♻️ Added a lib to avoid using strings to indicate the fieldname we want to use the RealmQuery

--- app/src/main/java/studio/lunabee/arn/db/AnimeListDao.kt
+++ app/src/main/java/studio/lunabee/arn/db/AnimeListDao.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData
 import com.zhuinden.monarchy.Monarchy
 import io.realm.kotlin.where
 import studio.lunabee.arn.vo.animelist.AnimeListItem
+import studio.lunabee.arn.vo.animelist.AnimeListItemFields
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -28,19 +29,19 @@ class AnimeListDao @Inject constructor(private val monarchy: Monarchy) {
 
     fun findByUserId(userId: String): LiveData<List<AnimeListItem>> {
         return monarchy.findAllCopiedWithChanges({ realm ->
-            realm.where<AnimeListItem>().equalTo("userId", userId)
+            realm.where<AnimeListItem>().equalTo(AnimeListItemFields.USER_ID, userId)
         })
     }
 
     fun findByAnimeId(animeId: String): LiveData<List<AnimeListItem>> {
         return monarchy.findAllCopiedWithChanges({ realm ->
-            realm.where<AnimeListItem>().equalTo("animeId", animeId)
+            realm.where<AnimeListItem>().equalTo(AnimeListItemFields.ANIME_ID, animeId)
         })
     }
 
     fun findById(id: String): LiveData<List<AnimeListItem>> {
         return monarchy.findAllCopiedWithChanges({ realm ->
-            realm.where<AnimeListItem>().equalTo("id", id)
+            realm.where<AnimeListItem>().equalTo(AnimeListItemFields.ID, id)
         })
     }
 }
--- app/src/main/java/studio/lunabee/arn/db/UserDao.kt
+++ app/src/main/java/studio/lunabee/arn/db/UserDao.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.LiveData
 import com.zhuinden.monarchy.Monarchy
 import io.realm.kotlin.where
 import studio.lunabee.arn.vo.user.User
+import studio.lunabee.arn.vo.user.UserFields
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,13 +17,13 @@ class UserDao @Inject constructor(private val monarchy: Monarchy) {
 
     fun findByNick(nickname: String): LiveData<List<User>> {
         return monarchy.findAllCopiedWithChanges { realm ->
-            realm.where<User>().like("nickName", nickname)
+            realm.where<User>().like(UserFields.NICK_NAME, nickname)
         }
     }
 
     fun findById(id: String): LiveData<List<User>> {
         return monarchy.findAllCopiedWithChanges { realm ->
-            realm.where<User>().equalTo("id", id)
+            realm.where<User>().equalTo(UserFields.ID, id)
         }
     }
 }
